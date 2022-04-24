4496c850c17bd98083a9be8aaa31a48a7c5fa66a
Aldi Aswari <aldiaswari6@gmail.com>
2022-04-25 05:34:10 +0700

    update

--- core/src/main/java/com/cuplix/cupita/core/di/DatabaseModule.kt
+++ core/src/main/java/com/cuplix/cupita/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,10 +19,16 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): MovieDatabase = Room.databaseBuilder(
-        context,
-       MovieDatabase::class.java, "Movie.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): MovieDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("aldiaswari".toCharArray())
+        val factory = SupportFactory(passphrase)
+
+        return Room.databaseBuilder(
+            context,
+            MovieDatabase::class.java, "Movie.db"
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
+
+    }
 
     @Provides
     fun provideTourismDao(database: MovieDatabase): MovieDao = database.movieDao()
--- core/src/main/java/com/cuplix/cupita/core/di/NetworkModule.kt
+++ core/src/main/java/com/cuplix/cupita/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -18,10 +19,19 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val hostname = "*.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
+
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(125, TimeUnit.SECONDS)
             .readTimeout(125, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
